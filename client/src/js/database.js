25f2264e5f1bf5c1e18bf59e213338e477ab471e
AlanDBalan <132862280+AlanDBalan@users.noreply.github.com>
2023-07-19 20:03:23 -0500

    Fixed more console errors
    Resolved putDB function error so content isn't undefined

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,6 +1,6 @@
 import { openDB } from 'idb';
 
-const initdb = async () =>
+const initdb = async () => {
   openDB('jate', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
@@ -11,20 +11,24 @@ const initdb = async () =>
       console.log('jate database created');
     },
   });
+};
 
-const putDb = async (content) => {
+const putDb = async (content,) => {
   const db = await openDB('jate', 1);
   const tx = db.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  await store.put(content);
-  await tx.done;
+  await store.put({content});
 };
+  
+  // When the editor is ready, set the value to whatever is stored in indexeddb.
+  // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
+  const getDb = async () => {
+    const db = await openDB('jate', 1);
+    const tx = db.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    await store.getAll(); // Add the 'return' statement here to return the data
+  };
 
-const getDb = async () => {
-  const db = await openDB('jate', 1);
-  const tx = db.transaction('jate', 'readonly');
-  const store = tx.objectStore('jate');
-  return store.getAll();
-};
+  initdb();
 
 export { putDb, getDb, initdb };
